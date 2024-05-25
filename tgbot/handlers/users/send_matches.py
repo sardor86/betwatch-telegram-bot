0709ee0f461e35e8031253fd3ad4b4befc533392
sardor86 <115871893+sardor86@users.noreply.github.com>
2024-05-25 21:19:25 +0500

    Update send_matches.py

--- tgbot/handlers/users/send_matches.py
+++ tgbot/handlers/users/send_matches.py
@@ -66,42 +66,45 @@ async def send_message(message: Message, match: dict):
 
 async def parser(message: Message):
     while True:
-        await asyncio.sleep(60)
+        try:
+            await asyncio.sleep(60)
 
-        logger.info('check is parser working')
-        if json.loads(await message.bot.redis.get(f'bot-{message.from_user.id}')) == 'stop':
-            break
+            logger.info('check is parser working')
+            if json.loads(await message.bot.redis.get(f'bot-{message.from_user.id}')) == 'stop':
+                break
 
-        logger.info('get all matches')
-        await message.bot.parser.get_all_matches()
+            logger.info('get all matches')
+            await message.bot.parser.get_all_matches()
 
-        logger.info('get old and new matches info')
-        old_matches_list = json.loads(await message.bot.redis.get(f'parser-{message.from_user.id}'))
-        new_matches_list = message.bot.parser.matches
+            logger.info('get old and new matches info')
+            old_matches_list = json.loads(await message.bot.redis.get(f'parser-{message.from_user.id}'))
+            new_matches_list = message.bot.parser.matches
 
-        logger.info('filter matches')
-        matches_list = await sort_matches(old_matches_list, new_matches_list)
+            logger.info('filter matches')
+            matches_list = await sort_matches(old_matches_list, new_matches_list)
 
-        logger.info('send new matches')
-        for new_matches in matches_list['new_matches']:
-            await send_message(message, new_matches)
+            logger.info('send new matches')
+            for new_matches in matches_list['new_matches']:
+                await send_message(message, new_matches)
 
-        logger.info('check and send old matches')
-        filters = json.loads(await message.bot.redis.get(f'filter-{message.from_user.id}'))
-        for match in matches_list['old_matches']:
-            old_match = old_matches_list[str(match['id'])]
-            result_runners = []
-            for runner in match['runners']:
-                old_runner = await get_old_data(runner, old_match['runners'])
-                if old_runner:
-                    change_percent = runner['percentage'] - old_runner['percentage']
-                    if filters['up_from_percentage'] <= change_percent <= filters['up_to_percentage']:
-                        runner['change_percent'] = change_percent
+            logger.info('check and send old matches')
+            filters = json.loads(await message.bot.redis.get(f'filter-{message.from_user.id}'))
+            for match in matches_list['old_matches']:
+                old_match = old_matches_list[str(match['id'])]
+                result_runners = []
+                for runner in match['runners']:
+                    old_runner = await get_old_data(runner, old_match['runners'])
+                    if old_runner:
+                        change_percent = runner['percentage'] - old_runner['percentage']
+                        if filters['up_from_percentage'] <= change_percent <= filters['up_to_percentage']:
+                            runner['change_percent'] = change_percent
+                            result_runners.append(runner)
+                    else:
                         result_runners.append(runner)
-                else:
-                    result_runners.append(runner)
-            result_match = match
-            result_match['runners'] = result_runners
-            await send_message(message, result_match)
-        logger.info('save new matches list to redis')
-        await message.bot.redis.set(f'parser-{message.from_user.id}', json.dumps(new_matches_list))
+                result_match = match
+                result_match['runners'] = result_runners
+                await send_message(message, result_match)
+            logger.info('save new matches list to redis')
+            await message.bot.redis.set(f'parser-{message.from_user.id}', json.dumps(new_matches_list))
+        except:
+            pass
